a481b57400931780ab4262a95b4f374f032c8bef
Black996 <rharasis@yahoo.com>
2021-04-24 19:47:52 +0300

    pages and routing

---
+++ pages/about/about.page.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is an app to find the latest Rock and other musical events</p>
+            <p>Version: 1.0.0</p>
+        </div>
+    )
+}
